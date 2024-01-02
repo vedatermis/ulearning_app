0ce90e6c687d69c586b79f29cf883290e289c51b
Vedat ERMİŞ <vedatermis@hotmail.com>
2024-01-02 13:57:11 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/app_events.dart';
+import 'package:ulearning_app/app_states.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_bloc.dart';
+import 'package:ulearning_app/pages/welcome/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => WelcomeBloc()),
+        BlocProvider(create: (context) => AppBlocs()),
+      ],
+      child: ScreenUtilInit(
+        builder: (context, child) => MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const Welcome(),
+          routes: {"myHomePage": (context) => const MyHomePage()},
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("dsdf"),
+      ),
+      body: Center(
+        child: BlocBuilder<AppBlocs, AppStates>(
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  BlocProvider.of<AppBlocs>(context).state.counter.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            heroTag: "h1",
+            onPressed: () =>
+                BlocProvider.of<AppBlocs>(context).add(Increment()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            heroTag: "h2",
+            onPressed: () =>
+                BlocProvider.of<AppBlocs>(context).add(Decrement()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
